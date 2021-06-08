d610ef8df82039ea18b019491c6aa02e5cdb0d42
Jorge1205 <85584139+Jorge1205@users.noreply.github.com>
2021-06-08 14:31:24 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+
+
+<script src="jquery-3.2.1.min.js"></script>
+<script src="lunax.js"></script>
+
+
+<div id="lunaradiov3" style="width:100%; height:600px;background: #000000 url(https://i.imgur.com/CTQdbqh.jpg) bottom / cover no-repeat;">Cargando ...</div>	
+
+<script>
+		//https://gist.github.com/jimmywarting/ac1be6ea0297c16c477e17f8fbe51347
+		$("#lunaradiov3").lunaradio({
+			userinterface: "big", 
+			backgroundcolor: "",
+			fontcolor: "#ffffff",
+			hightlightcolor: "#00ff7b", 
+fontname: "Anton", 
+googlefont: "Anton&display=swap", 
+fontratio: "0.4", 
+			radioname: "TEST RADIO", 
+			scroll: "true", 
+			coverimage: "https://uploads-ssl.webflow.com/574c7f22e4fba5b17220387f/5769c7f1225f3f9e094cfc1c_CD-AndyTorresPalabrasDeVida-tn.jpg", 
+			coverstyle: "animated",
+			usevisualizer: "real", 
+			visualizertype: "4", 
+			itunestoken: "1000lIPN", 
+			metadatatechnic: "", 
+			ownmetadataurl: "", 
+			corsproxy: "https://cors-anywhere.herokuapp.com/",
+			usestreamcorsproxy: "true",
+			streamurl: "https://server1.radios-streaming.com/radio/8040", 
+			streamtype: "icecast2", 
+			icecastmountpoint: "/live", 
+			radionomyid: "", 
+			radionomyapikey: "",
+			radiojarid: "",  
+			radiocoid: "",
+			shoutcastpath: "",
+			shoutcastid: "", 
+			streamsuffix: "", 
+			metadatainterval: "5000", 
+			volume: "100",
+			autoplay: "true",
+			debug: "false"
+		});
+	</script>
